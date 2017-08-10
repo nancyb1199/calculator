50aef6419ab6d0ca2ff10378f845f77ac3c62805
Nancy <belscher@nancys-mbp.attlocal.net>
2017-08-09 21:06:20 -0400

    getting events fired and switch statement working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 console.log("working");
+
+let calc = document.getElementById("container");
+calc.addEventListener("click", myCallBack);
+
+let result = "";
+
+function myCallBack(){
+  console.log("In myCallBack");
+  let key = event.target.id;
+  console.log(key);
+  handleKey(key);
+}
+
+function handleKey (key){
+switch (key) {
+  case 'plus':
+    result += " + ";
+    break;
+  case 'minus':
+    result += " - ";
+    break;
+  case 'times':
+    result += " x ";
+    break;
+  case 'divide':
+    result += " - ";
+    break;
+  case 'dot':
+    result += ".";
+    break;
+  case 'eq':
+    complete();
+    break;
+  case 'clear':
+    clearInput();
+    break;
+  default:
+    result += key;
+
+  } /* switch */
+  console.log(result);
+} /* handleKey */
